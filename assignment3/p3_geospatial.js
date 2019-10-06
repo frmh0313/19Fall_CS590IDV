d3a5d1f7cec7efdc621e137d880e3b60f0823b62
Myeonghan Ryu <rmh0313@gmail.com>
2019-10-05 22:50:46 -0400

    task2 draw nodes and edges - should color dots as its state

--- assignment3/p3_geospatial.js
+++ assignment3/p3_geospatial.js
@@ -1,72 +1,86 @@
 let dataSet;
-let bounds;
-let dots;
+let projection;
 
-async function draw() {
+async function drawMap() {
+  dataSet = await d3.json("./USAir97v2.json").then(airports => airports);
+  console.log(dataSet);
+  const width = 970;
+  const height = 610;
   let dimensions = {
-    width: 1100,
-    height: 700,
+    width: d3.min([900, window.innerWidth * 0.9]),
     margin: {
-      top: 15,
-      right: 15,
+      top: 10,
+      right: 40,
       bottom: 40,
       left: 60
     }
   };
 
-  dimensions.boundedHeight =
-    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
-  dimensions.boundedWidth =
-    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  let us = topojson.feature(states, states.objects.states_20m_2017);
+  projection = d3 //geoAlbersUsaPr()
+    .geoAlbersUsa()
+    .scale(1300)
+    .translate([487.5, 305]);
+  let path = d3.geoPath().projection(projection);
 
-  dataSet = await d3.json("./USAir97.json").then(data => data);
-
-  const nodes = dataSet.nodes;
-
-  const links = dataSet.links;
-
-  const wrapper = d3
+  let bounds = d3
     .select("#wrapper")
     .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height);
-
-  bounds = wrapper
-    .append("g")
-    .style(
-      "transform",
-      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
-    );
-
-  let projection = d3
-    .geoAlbersUsa()
-    .translate([dimensions.boundedWidth / 2, dimensions.boundedHeight / 2])
-    .scale([1280]);
-
-  let path = d3.geoPath();
-  // .projection(projection);
-
-  const us = await d3
-    .json("https://cdn.jsdelivr.net/npm/us-atlas@1/us/10m.json")
-    .then(us => {
-      bounds
-        .append("path")
-        .datum(topojson.mesh(us, us.objects.states))
-        .attr("fill", "none")
-        .attr("stroke", "#777")
-        .attr("stroke-width", 0.5)
-        .attr("stroke-linejoin", "round")
-        .attr("d", path);
-    });
-
-  bounds
-    .selectAll("circle")
-    .data(dataSet.nodes)
-    .join("circle")
-    .attr("r", 5)
+    // .attr("viewBox", [0, 0, 975, 610]);
+    .attr("width", 975)
+    .attr("height", 610)
     .attr(
       "transform",
-      d => `translate(${projection[(d.longitude, d.latitude)]})`
+      `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
     );
+
+  let path_states = bounds
+    .selectAll(".state")
+    .data(us.features)
+    .join("path")
+    .attr("class", "state")
+    .attr("d", path);
+
+  let airports = dataSet.nodes.map(airport => ({
+    state: airport.state,
+    coordinates: projection([airport.longitude, airport.latitude])
+  }));
+  console.log("airports");
+  console.log(airports);
+
+  let nullNodes = (() => {
+    let nulls = [];
+    airports.forEach((d, i) => {
+      if (d.coordinates == null) nulls.push(i);
+    });
+    return nulls;
+  })();
+
+  console.log(nullNodes);
+
+  // console.log(dataSet.links);
+  let nodes = bounds
+    .selectAll("circle")
+    .data(airports.filter(d => d.coordinates != null))
+    .join("circle")
+    .attr("cx", d => d.coordinates[0])
+    .attr("cy", d => d.coordinates[1])
+    .attr("r", 2);
+
+  let links = bounds
+    .selectAll("line")
+    .data(
+      dataSet.links.filter(
+        d => !nullNodes.includes(d.source) && !nullNodes.includes(d.target)
+      )
+    )
+    .join("line")
+    .attr("x1", d => airports[d.source].coordinates[0])
+    .attr("y1", d => airports[d.source].coordinates[1])
+    .attr("x2", d => airports[d.target].coordinates[0])
+    .attr("y2", d => airports[d.target].coordinates[1])
+    .attr("stroke", "#00f")
+    .attr("stroke-opacity", 0.6)
+    .attr("stroke-width", d => d.value * 3);
 }
-draw();
+drawMap();
